571028dc2d1435c2d7ffd814856582953be569b4
thakurlakshay2 <thakurlakshay2@gmail.com>
2024-04-15 17:36:37 +0530

    small change

---
+++ src/ObserverPattern/Observable/StocksObservable.java
@@ -0,0 +1,15 @@
+package ObserverPattern.Observable;
+
+import ObserverPattern.Observer.NotificationAlertObserver;
+
+public interface StocksObservable {
+    public void add(NotificationAlertObserver observer);
+
+    public void remove(NotificationAlertObserver observer);
+
+    public void notifySubscribers();
+
+    public void setStockCount(int newStockAdded);
+
+    public int getStockCount();
+}
